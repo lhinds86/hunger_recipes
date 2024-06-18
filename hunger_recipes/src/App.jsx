bfecd691b09e4e4c84692eeb8da548a0d3a95840
jslempa <jslempa@gmail.com>
2024-06-18 16:59:42 -0400

    started axios call

--- hunger_recipes/src/App.jsx
+++ hunger_recipes/src/App.jsx
@@ -1,10 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
+// import Header from './components/Header'
+// import Main from './components/Main'
 
 function App() {
+  
+  const BASE_URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
+
+  const [meals, setMeals] = useState([])
+
+  useEffect(() => {
+    const getMeals = async () => {
+      const res = await axios.get(`${BASE_URL}chicken`)
+      let mealList = res.data.meals
+      console.log(mealList)
+      // setMeals(mealList)
+    }
+    getMeals()    
+  })
+  
 
   return (
-    <div></div>
+    <div>
+      {/* <Header/>
+      <Main/> */}
+    </div>
   )
 }
 
